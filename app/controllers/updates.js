dd79526013066c22c25e1f7191dede3c8cbea73e
Sam Tsai <TsaiSa@ae.com>
2015-02-12 11:20:26 -0500

    Handle 'publishUpdate' action

--- app/controllers/updates.js
+++ app/controllers/updates.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ArrayController.extend({
+
+  init: function() {
+    this.set('update', Ember.Object.create());
+  },
+  sortProperties: ['timestamp'],
+  sortAscending: false, // sorts post by timestamp
+  actions: {
+    publishUpdate: function() {
+      var newUpdate = this.store.createRecord('update', {
+        author: this.get('update.author'),
+        status: this.get('update.status'),
+        timestamp: new Date()
+      });
+      newUpdate.save();
+    }
+  }
+
 });
