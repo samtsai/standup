c300e989dbfb2a7a402b7753debf39263ac932c2
Sam Tsai <TsaiSa@ae.com>
2015-02-12 11:19:32 -0500

    Clean up model attributes

--- app/models/update.js
+++ app/models/update.js
@@ -2,5 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   author: DS.attr('string'),
-  update: DS.attr('string')
+  status: DS.attr('string'),
+  timestamp: DS.attr('number')
 });
